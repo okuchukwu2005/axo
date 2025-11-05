f5c38cc1f5bdbd54321152d06853d92483dbf68d
okuchukwu2005 <okuchukwu2005@gmail.com>
2025-11-05 01:58:46 +0100

    linking and compiling properly

--- compile.sh
+++
@@ -1,1 +0,0 @@
-gcc main.c -I forms/include -L . -lforms -lSDL2 -lSDL2_ttf -lSDL2_image -o main
--- forms/form.h
+++ forms/form.h
@@ -2,19 +2,19 @@
 #ifndef FORM_H
 #define FORM_H
 
-#include"core/parent.h"
-#include"core/graphics.h"
-#include"core/theme.h"
-#include"widgets/container.h"
-#include"widgets/entry.h"
-#include"widgets/drop.h"
-#include"widgets/radio.h"
-#include"widgets/textbox.h"
-#include"widgets/slider.h"
-#include"widgets/button.h"
-#include"widgets/text.h"
-#include"widgets/progress.h"
-#include"widgets/image.h"
-#include"core/app.h"
+#include"include/core/parent.h"
+#include"include/core/graphics.h"
+#include"include/core/theme.h"
+#include"include/widgets/container.h"
+#include"include/widgets/entry.h"
+#include"include/widgets/drop.h"
+#include"include/widgets/radio.h"
+#include"include/widgets/textbox.h"
+#include"include/widgets/slider.h"
+#include"include/widgets/button.h"
+#include"include/widgets/text.h"
+#include"include/widgets/progress.h"
+#include"include/widgets/image.h"
+#include"include/core/app.h"
 
 #endif // FORM_H
